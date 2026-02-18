004a2d121ce83a3024875d3a658c94db98241c2a
Ravindra Nayak <ravindra.nayak@outlook.com>
2026-02-19 00:34:20 +0100

    Update item names and descriptions in data.js

--- docs/data.js
+++ docs/data.js
@@ -12,7 +12,7 @@ const stamps = [
         desc: "RN4057: Thick stockbook with stamp collection various.Check the photos. What you see is what you get.(+ shipping Inside India in April 2026).Thick album for free" 
     },
     { 
-        name: "Dutch Antilles and Suriname FDC's", 
+        name: "350 different Dutch Antilles and Suriname FDC's", 
         country: "Dutch Antilles and Suriname", 
         year: "1965+", 
         priceINR: 17500, 
@@ -20,7 +20,7 @@ const stamps = [
         folder: "FDC",
         imageCount: 23,
         //images: ["https://picsum.photos/400/300?random=3"],
-        desc: "RN4059:3 Luxureous Importa albums in excellent condition with collection about 350 FDC covers Dutch Antilles and Suriname. Check the photos to see a small part, but there is a lot more than you can see here.Price: Rs.50 per cover(Total will be 17500/-). Three albums for free + shipping inside India in April 2026" 
+        desc: "RN4059:3 Luxureous Importa albums in excellent condition with collection about 350 FDC covers Dutch Antilles and Suriname. Check the photos to see a small part, but there is a lot more than you can see here.Three albums for FREE + shipping inside India in April 2026" 
     },
     { 
         name: "TinTin FDC and MS", 
@@ -34,7 +34,7 @@ const stamps = [
         desc: "RN4043: FDC and miniature sheet from Netherlands.(Free letter post shipping worldwide)"
     },
     { 
-        name: "60 FDC's", 
+        name: "60 FDC's from Switzerland", 
         country: "Switzerland", 
         year: "various", 
         priceINR: 3000, 
@@ -45,13 +45,13 @@ const stamps = [
         desc: "RN4051: 60 different FDC from Switzerland. (+ shipping inside India in April 2026)"
     },
     { 
-        name: "USED stamps lot", 
+        name: "Used stamps lot", 
         country: "Different countries", 
         year: "various", 
         priceINR: 899, 
         isSoldOut: false,
         folder: "RN4056",
-        imageCount: 1,
+        imageCount: 4,
         //images: ["https://picsum.photos/400/300?random=3"],
         desc: "RN4056: Used stamps from around the world. ( Free letter post shipping worldwide from Netherlands)"
     }
