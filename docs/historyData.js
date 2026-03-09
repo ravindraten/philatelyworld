4c155ad1cc48c19851046a9cbcaf0b45b52e0821
Ravindra  Nayak <ravindra.nayak@outlook.com>
2026-03-09 22:16:31 +0100

    update

--- docs/historyData.js
+++ docs/historyData.js
@@ -1,5 +1,5 @@
 const PHILATELY_HISTORY = {
-    "3-1": {
+    "3-9": {
             title: "Queen Victoria (1819-1901)",
             date: "March 1, 1876",
             desc: "The 1881 'Penny Lilac' was a revolutionary British stamp created to serve both postal and fiscal purposes, featuring the new 'Postage and Inland Revenue' inscription. Printed by De La Rue using 'fugitive' ink to prevent fraud, it is most famous for its two versions: the scarcer Die I (14 dots) and the incredibly common Die II (16 dots), of which over 33 billion were produced. Its immense circulation led to frequent use with departmental overprints and commercial 'perfins,' making it a foundational piece of Victorian philatelic history.",
@@ -13,7 +13,7 @@ const PHILATELY_HISTORY = {
         img1: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4a/Stamp_US_1847_5c.jpg/500px-Stamp_US_1847_5c.jpg",
         img2: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4a/Stamp_US_1847_5c.jpg/500px-Stamp_US_1847_5c.jpg"
     },
-    "3-3": {
+    "9-3": {
         title: "First Stamps of Japan",
         date: "March 3, 1871",
         desc: "Japan issued its first stamps, known as the 'Dragon' series. They were printed on Japanese paper (washi) and featured no perforations.",
