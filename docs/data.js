a0ed6617b3a33320bcb52de06f4e4d4d4b1f6518
Ravindra  Nayak <ravindra.nayak@outlook.com>
2026-08-14 11:43:59 +0200

    update

--- docs/data.js
+++ docs/data.js
@@ -5,7 +5,7 @@ const stamps = [
         country: "Various Countries",
         year: "<b>Year</b>: 1960+",
         priceINR: 699,
-        isSoldOut: false,
+        isSoldOut: true,
         freeTrackedShipping: false,
         freeLetterPostShipping: true,
         folder: "D96",
