b526f5666b66ac2207ab7f0c54b23c7fbd2f6473
Ravindra  Nayak <ravindra.nayak@outlook.com>
2026-08-18 14:21:53 +0200

    new listing RN4176

--- docs/data.js
+++ docs/data.js
@@ -8,6 +8,17 @@ const stamps = [
         isSoldOut: false,
         freeTrackedShipping: false,
         freeLetterPostShipping: true,
+        folder: "D107",
+        imageCount: 1,
+        desc: "RN4176: 57 stamps from around the world on theme Birds. Check the photos. What you see is what you get."
+    }, {
+        name: "Mixed stamps from from the world on theme - Birds.",
+        country: "Various Countries",
+        year: "<b>Year</b>: 1960+",
+        priceINR: 599,
+        isSoldOut: false,
+        freeTrackedShipping: false,
+        freeLetterPostShipping: true,
         folder: "D106",
         imageCount: 1,
         desc: "RN4175: 60 stamps from around the world on theme Birds. Check the photos. What you see is what you get."
